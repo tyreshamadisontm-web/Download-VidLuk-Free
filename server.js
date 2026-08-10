166da643cb9d6336442bcc31d614834648c0a515
tyreshamadisontm-web <tyresha.madison.tm@gmail.com>
2026-08-10 08:08:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -16,8 +16,24 @@ const WORK_DIR = path.join(os.tmpdir(), "vidluk");
 fs.mkdirSync(WORK_DIR, { recursive: true });
 
 app.use(express.json({ limit: "1mb" }));
-app.use(express.static(__dirname));
 
+app.use((req, res, next) => {
+  res.setHeader("X-Content-Type-Options", "nosniff");
+  res.setHeader("X-Frame-Options", "DENY");
+  res.setHeader("Referrer-Policy", "strict-origin-when-cross-origin");
+  res.setHeader(
+    "Permissions-Policy",
+    "camera=(), microphone=(), geolocation=()"
+  );
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; media-src 'self' blob:; connect-src 'self'; frame-ancestors 'none'"
+  );
+
+  next();
+});
+
+app.use(express.static(__dirname));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
